4ffe61ba63551cca66f5866485ec1d5ed517d701
Swift-Jr <robguardjr@gmail.com>
2018-11-15 09:06:19 +0000

    Touch controls for menu

--- src/containers/App/AppMenu.jsx
+++ src/containers/App/AppMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import TouchEvent from '../../system/TouchEvent';
 
 import {PlayMatch} from '../PlayMatch';
 
@@ -12,52 +13,50 @@ export class AppMenu extends React.Component {
     this.state = {
       visible: props.visible || false
     }
-
-    this.handleLinkClick = this
-      .handleLinkClick
-      .bind(this);
   }
 
-  toggleMenu() {
+  toggleMenu = () => {
     const {onToggle} = this.props;
     this.setState({
       visible: !this.state.visible
     }, () => onToggle && onToggle(this.state.visible));
   }
 
-  handleLinkClick(e) {
-    this.setState({visible: false});
-    this.toggleMenu();
+  handleLinkClick = (e) => {
+    //this.setState({visible: false});
+    //this.toggleMenu();
   }
 
   render() {
     const {visible} = this.state;
 
-    return (<nav>
-      <ul className={visible
-          ? 'showMenu'
-          : 'hideMenu'}>
-        <li>
-          <Link to="/myleagues" onClick={this.handleLinkClick}>My Leagues</Link>
-        </li>
-        <li>
-          <Link to="/results" onClick={this.handleLinkClick}>Results</Link>
-        </li>
-        <li>
-          <Link to="/headtohead" onClick={this.handleLinkClick}>Head to Head</Link>
-        </li>
-        <li>
-          <Link to="/scorecard" onClick={this.handleLinkClick}>Scorecard</Link>
-        </li>
-      </ul>
-      <img src={iconMenu} onClick={() => this.toggleMenu()} alt="Menu Toggle"/> {
-        visible
-          ? <div className={`fixedBottom play-match reverseVisible`}>
-              <PlayMatch onClose={() => this.toggleMenu()}></PlayMatch>
-            </div>
-          : null
-      }
-    </nav>)
+    return (<TouchEvent onSwipeDown={this.toggleMenu} onSwipeUp={this.toggleMenu} onTap={this.toggleMenu}>
+      <nav>
+        <ul className={visible
+            ? 'showMenu'
+            : 'hideMenu'}>
+          <li>
+            <Link to="/myleagues" onClick={this.handleLinkClick}>My Leagues</Link>
+          </li>
+          <li>
+            <Link to="/results" onClick={this.handleLinkClick}>Results</Link>
+          </li>
+          <li>
+            <Link to="/headtohead" onClick={this.handleLinkClick}>Head to Head</Link>
+          </li>
+          <li>
+            <Link to="/scorecard" onClick={this.handleLinkClick}>Scorecard</Link>
+          </li>
+        </ul>
+        <img src={iconMenu} alt="Menu Toggle"/> {
+          visible
+            ? <div className={`fixedBottom play-match reverseVisible`}>
+                <PlayMatch onClose={() => this.toggleMenu()}></PlayMatch>
+              </div>
+            : null
+        }
+      </nav>
+    </TouchEvent>)
   }
 }
 
